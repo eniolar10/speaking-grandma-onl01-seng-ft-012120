23410271b6c8ca9938c3d3e02286e9ada156b781
Ola Enol <eniolar10@gmail.com>
2020-01-30 18:17:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma (phrase)
   
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "WHAT DID YOU EAT TODAY?" || phrase == "WHAT?"
+  elsif phrase == 
     "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
@@ -25,4 +25,4 @@ end
 
 speak_to_grandma("I LOVE YOU GRANDMA")
 
-speak_to_grandma("Hi Nana, how are you?")
+speak_to_grandma("AAAAAAAAAAAAA")
